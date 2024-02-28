6b2c68ce9e933faef912aa3068605ebbebd7a09c
bananana0118 <bananana0118@gmail.com>
2024-02-28 11:01:59 +0900

    feat : 라우터설정 및 레이아웃

---
+++ krampoline/src/Layout.js
@@ -0,0 +1,43 @@
+//import { useState } from 'react';
+import styled from "styled-components";
+import "./App.css";
+//import { instance } from "./api.js";
+
+const Layout = ()=> {
+    // const [apiTest, setApiTest] = useState(false);
+    // const [dbTest, setDbTest] = useState(false);
+
+    // const handleApiOnclick = async () => {
+    //     try {
+    //         await instance.get("/test");
+    //         setApiTest(true);
+    //     } catch(err) {
+    //         console.log(err);
+    //     }
+
+    // }
+
+    // const handleDbOnClick = async () => {
+    //     try {
+    //         await instance.get("/db");
+    //         setDbTest(true);
+    //     } catch(err) {
+    //         console.log(err);
+    //     }
+    // }
+
+    return (
+        <div className="App">
+            <Container></Container>
+        </div>
+    );
+}
+
+const Container = styled.div`
+    width: 375px;
+    height: 100vh;
+    background-color: white;
+    border: 1px solid black;
+    box-sizing: border-box;
+`;
+export default Layout;
