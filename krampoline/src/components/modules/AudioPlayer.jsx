4c1fdd59af50166d6bf603a1f394b83f3b937a8b
myungju <myungju030@gmail.com>
2024-02-29 01:11:27 +0900

    feat : 오디오 플레이어 기능 구현

---
+++ krampoline/src/components/modules/AudioPlayer.jsx
@@ -0,0 +1,132 @@
+import { useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import speech from "assets/speech.mp3";
+import stop from "assets/stop.svg";
+import start from "assets/play.svg";
+
+const AudioPlayer = () => {
+  const audioRef = useRef(new Audio(speech));
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const progressBarContainerRef = useRef(null);
+
+  const togglePlayPause = () => {
+    const prevValue = isPlaying;
+    setIsPlaying(!prevValue);
+    if (!prevValue) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  };
+
+  useEffect(() => {
+    const audio = audioRef.current;
+
+    const onLoadedMetadata = () => {
+      setDuration(audio.duration);
+    };
+
+    const onTimeUpdate = () => {
+      setCurrentTime(audio.currentTime);
+    };
+
+    const onEnded = () => {
+      setIsPlaying(false);
+      alert("스탬프 발급완료");
+    };
+
+    audio.addEventListener("loadedmetadata", onLoadedMetadata);
+    audio.addEventListener("timeupdate", onTimeUpdate);
+    audio.addEventListener("ended", onEnded);
+
+    return () => {
+      audio.removeEventListener("loadedmetadata", onLoadedMetadata);
+      audio.removeEventListener("timeupdate", onTimeUpdate);
+      audio.removeEventListener("ended", onEnded);
+    };
+  }, []);
+
+  const changeAudioProgress = (e) => {
+    const width = progressBarContainerRef.current.offsetWidth; // 전체 진행률 바의 너비
+    const clickX = e.nativeEvent.offsetX; // 클릭한 위치
+    const duration = audioRef.current.duration;
+    audioRef.current.currentTime = (clickX / width) * duration;
+  };
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  };
+
+  return (
+    <Container>
+      <Wrapper>
+        <Button onClick={togglePlayPause}>
+          {isPlaying ? <img src={stop} /> : <img src={start} />}
+        </Button>
+        <ProgressBarContainer
+          ref={progressBarContainerRef}
+          onClick={changeAudioProgress}
+        >
+          <ProgressBar currentTime={currentTime} duration={duration} />
+        </ProgressBarContainer>
+        <Time>{formatTime(duration)}</Time>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default AudioPlayer;
+
+const Container = styled.div`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 5.75rem;
+  display: flex;
+`;
+
+const Wrapper = styled.div`
+  width: ${(props) => props.theme.layout.minWidth.mobile};
+  height: 5.75rem;
+  padding: 0 1.5rem;
+  background-color: ${(props) => props.theme.colors.background.primary};
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Button = styled.span`
+  width: 1.5rem;
+  height: 1.5rem;
+  cursor: pointer;
+
+  > img {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+`;
+
+const ProgressBarContainer = styled.div`
+  width: 16rem;
+  height: 0.5rem;
+  background-color: #d9d9d9;
+  border-radius: 1rem;
+  cursor: pointer;
+`;
+
+const ProgressBar = styled.div`
+  height: 100%;
+  border-radius: 1rem;
+  background-color: ${(props) => props.theme.colors.main.primary};
+  width: ${(props) => (props.currentTime / props.duration) * 100}%;
+`;
+
+const Time = styled.span`
+  color: ${(props) => props.theme.font.colors.white};
+  font-size: ${(props) => props.theme.font.fontSize.small};
+`;
