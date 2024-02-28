064d025fe5af651845e36c6cd3773bb166b98bff
bananana0118 <bananana0118@gmail.com>
2024-02-28 20:02:13 +0900

    chore : dummy 데이터 추가

---
+++ krampoline/src/pages/dummy.js
@@ -0,0 +1,137 @@
+export const DUMMY = [
+    {
+        id: 1,
+        name: "제주 4·3 평화공원",
+        px: 164,
+        py: 61,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 2,
+        name: "관덕정",
+        px: 188,
+        py: 18,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 3,
+        name: "다랑쉬굴",
+        px: 247,
+        py: 65,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 4,
+        name: "곤을동 옛터",
+        px: 211,
+        py: 8,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 5,
+        name: "너븐숭이 기념관",
+        px: 267,
+        py: 8,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 6,
+        name: "터진목",
+        px: 287,
+        py: 71,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 7,
+        name: "한모살",
+        px: 247,
+        py: 113,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 8,
+        name: "현의합장묘",
+        px: 208,
+        py: 120,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 9,
+        name: "송령이골",
+        px: 178,
+        py: 96,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 10,
+        name: "정방폭포",
+        px: 141,
+        py: 145,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 11,
+        name: "백조일손지지",
+        px: 26,
+        py: 145,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 12,
+        name: "진아영 할머니 삶터",
+        px: 20,
+        py: 91,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 13,
+        name: "빌레못굴",
+        px: 62,
+        py: 55,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 14,
+        name: "영모원",
+        px: 113,
+        py: 35,
+        imageLink: "",
+        content: "",
+        address: ""
+    },
+    {
+        id: 15,
+        name: "정뜨르 비행장",
+        px: 141,
+        py: 22,
+        imageLink: "",
+        content: "",
+        address: ""
+    }
+];
