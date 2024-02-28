9811db0d34521e0443699d94831e2d697b352af4
bananana0118 <bananana0118@gmail.com>
2024-02-28 15:47:24 +0900

    style : color theme 설정

--- krampoline/src/styles/theme.js
+++ krampoline/src/styles/theme.js
@@ -1,9 +1,23 @@
 export const theme = {
     colors: {
-        primary: "blue",
-        secondary: "lightgray",
-        white: "#fff",
-        black: "#000",
+        main: {
+            primary: "#FB624D",
+            secondary: "#FF3F3F",
+            third: "#FFB2B2",
+        },
+        highlight: {
+            primary: "#FFB72B",
+            secondary: "#FDC966",
+            third: "#FFEAC0",
+        },
+        neutral: {
+            600: "#252936",
+            500: "#585D6F",
+            400: "#7E818C",
+            300: "#B0B2B9",
+            200: "DEE1EB",
+            100: "F0F4FF",
+        },
     },
     font: {
         family: "Spoqa Han Sans Neo",
@@ -12,15 +26,19 @@ export const theme = {
             medium: "1rem",
             large: "1.5rem",
         },
+        colors: {
+            white: "#fff",
+            black: "#000",
+        },
         fontStyle: {
             bold: 600,
             medium: 400,
         },
     },
     radius: {
-      small:"0.875rem",
-      medium:"1rem",
-      large:"1.125rem"
+        small: "0.875rem",
+        medium: "1rem",
+        large: "1.125rem",
     },
     layout: {
         minWidth: {
