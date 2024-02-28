672c9dc6c6aba615b49b124815361fc968aeec87
bananana0118 <bananana0118@gmail.com>
2024-02-28 15:04:46 +0900

    style : theme 파일 공통 스타일링 설정

--- krampoline/src/styles/theme.js
+++ krampoline/src/styles/theme.js
@@ -1,19 +1,30 @@
 export const theme = {
     colors: {
-      primary: "blue",
-      secondary: "lightgray",
-      white: "#fff",
-      black: "#000",
+        primary: "blue",
+        secondary: "lightgray",
+        white: "#fff",
+        black: "#000",
     },
     font: {
-      family: "Spoqa Han Sans Neo",
-      baseSize: "16px",
+        family: "Spoqa Han Sans Neo",
+        fontSize: {
+            small: "0.75rem",
+            medium: "1rem",
+            large: "1.5rem",
+        },
+        fontStyle: {
+            bold: 600,
+            medium: 400,
+        },
     },
-    container: {
-      maxWidth: {
-        pc: "1200px",
-        tablet: "700px",
-        mobile: "500px",
-      },
+    radius: {
+      small:"0.875rem",
+      medium:"1rem",
+      large:"1.125rem"
     },
-  }
+    layout: {
+        minWidth: {
+            mobile: "375px",
+        },
+    },
+};
